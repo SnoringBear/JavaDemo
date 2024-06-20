3d48a262f1bd73ca90cb5c021b978060797d906b
andy <andy@gmail.com>
2024-06-20 17:58:41 +0800

    add

---
+++ src/main/java/ch01/SteamTest.java
@@ -0,0 +1,17 @@
+package ch01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SteamTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        long count = map.values().stream()
+                .filter(item -> item != null && item.equals("1"))
+                .count();
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/ch02/ArraysTest.java
@@ -0,0 +1,48 @@
+package ch02;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        sort2();
+    }
+
+    public static void sort1() {
+        // Java 类型  变量
+        // Golang 变量  类型
+        int[] a = {1, 6, 3, 9, 5};
+        // 会改变原数组
+        Arrays.parallelSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void sort2() {
+        S[] XX = {new S(1), new S(6), new S(3), new S(9), new S(5)};
+        Arrays.parallelSort(XX,Comparator.comparingInt(S::getA));
+        System.out.println(Arrays.toString(XX));
+    }
+}
+
+class S {
+    int a;
+
+    public S(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public String toString() {
+        return "S{" +
+                "a=" + a +
+                '}';
+    }
+}
