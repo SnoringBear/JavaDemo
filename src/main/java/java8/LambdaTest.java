e2015f05669be1a3c354e0248324b1823b34eaa5
andy <andy@gmail.com>
2024-06-24 13:57:52 +0800

    Lambda表达式

--- src/main/java/java8/LambdaTest.java
+++ src/main/java/java8/LambdaTest.java
@@ -2,11 +2,15 @@ package java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class LambdaTest {
     public static void main(String[] args) {
-
+        Object o = test2(LambdaTest::test3, 1);
+        System.out.println(o);
     }
+
+
 
     public void test1(){
         List<String> names = Arrays.asList("张三", "李四", "王五");
@@ -16,5 +20,12 @@ public class LambdaTest {
         // Predicate->boolean test(T t)
         // Supplier -> T get()
         names.forEach(name -> System.out.println(name));
+    }
+
+    public static String test2(Function<Integer, String> function, Integer t){
+        return function.apply(t);
+    }
+    public static String test3(int i){
+        return "hello"+i;
     }
 }
