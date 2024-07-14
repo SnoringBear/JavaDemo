33210c5b801b861e4017089852d54923977f1ce8
andy <andy@gmail.com>
2024-07-14 21:11:11 +0800

    add

---
+++ src/main/java/number/LongSort.java
@@ -0,0 +1,11 @@
+package number;
+
+import java.util.Arrays;
+
+public class LongSort {
+    public static void main(String[] args) {
+        long[] arr = {6,4,1,9};
+        long[] array = Arrays.stream(arr).sorted().toArray();
+        Arrays.stream(array).forEach(System.out::println);
+    }
+}
