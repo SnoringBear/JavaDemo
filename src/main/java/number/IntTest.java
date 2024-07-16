b1aefd0c0c14461cd992bc3006b847321f3ec1ba
andy <andy@gmail.com>
2024-07-16 10:38:10 +0800

    add

---
+++ src/main/java/number/IntTest.java
@@ -0,0 +1,11 @@
+package number;
+
+public class IntTest {
+    public static void main(String[] args) {
+        int dayMaxDrop = 10;
+        int dropAllNum = 50;
+        int v = (int) ((double) dayMaxDrop / dropAllNum * 100);
+        System.out.println(v);
+
+    }
+}
