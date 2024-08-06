4f5704ce38ec5ca4ebf96a45d15d281bdd9a8109
andy <andy@gmail.com>
2024-08-06 21:30:21 +0800

    add

--- src/main/java/List/SortTest.java
+++ src/main/java/List/SortTest.java
@@ -6,8 +6,8 @@ public class SortTest {
     public static void main(String[] args) {
         long[] array = {5L, 1L, 4L, 2L, 8L};
 
-        Arrays.sort(array, (x, y) -> Long.compare(x, y));
-
-        System.out.println(Arrays.toString(array));
+//        Arrays.sort(array, (x, y) -> Long.compare(x, y));
+//
+//        System.out.println(Arrays.toString(array));
     }
 }
