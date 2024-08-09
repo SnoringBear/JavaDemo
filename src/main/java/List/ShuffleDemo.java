acfe63af58a363da73f2a03fdead6faf62da33e2
andy <andy@gmail.com>
2024-08-09 15:28:49 +0800

    add

---
+++ src/main/java/List/ShuffleDemo.java
@@ -0,0 +1,36 @@
+package List;
+
+import java.util.*;
+
+public class ShuffleDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            test1();
+        }
+    }
+    public static void test1(){
+        Set<Long> subListK = new TreeSet<Long>();
+        if (subListK.size() != 5) {
+            // 补充虚拟玩家
+            List<Long> virtualPlayers = drawVirtualPlayers();
+            Collections.shuffle(virtualPlayers);
+            System.out.println(virtualPlayers);
+            if (virtualPlayers.contains(100)) {
+                virtualPlayers.remove(100);
+            }
+            subListK.addAll(virtualPlayers);
+
+        }
+        List<Long> list = new ArrayList<>(subListK);
+        list = list.subList(0, Math.min(list.size(), 5));
+        System.out.println(list);
+    }
+
+    public static List<Long> drawVirtualPlayers(){
+        List<Long> list = new ArrayList<>();
+        for (long i=0;i<20;i++){
+            list.add(i);
+        }
+        return list;
+    }
+}
