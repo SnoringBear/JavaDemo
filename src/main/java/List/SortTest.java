48348808bc4b6a9c0cba11eee4fc5c0efe68bd45
anye <anye@gmail.com>
2024-08-16 21:41:35 +0800

    add

--- src/main/java/List/SortTest.java
+++ src/main/java/List/SortTest.java
@@ -1,13 +1,25 @@
 package List;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class SortTest {
     public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1(){
         long[] array = {5L, 1L, 4L, 2L, 8L};
 
 //        Arrays.sort(array, (x, y) -> Long.compare(x, y));
 //
 //        System.out.println(Arrays.toString(array));
+    }
+
+    public static void test2(){
+        List<Integer> list = Arrays.asList(8,2,5,4);
+        Collections.sort(list);
+        list.forEach(System.out::println);
     }
 }
